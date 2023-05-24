79ebbd6e15c7b8ee83652ae93f145fe0cde4af34
Kauã Cavalcante <kauacavalcante777@gmail.com>
2023-05-24 11:22:41 -0300

    style: add sass and change font family

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,17 +1,7 @@
-import { useEffect } from "react";
-
 const TaskItem = ({ task }) => {
-    useEffect(() => {
-        console.log("component was mounted!");
-
-        return () => {
-            console.log("i will unmount");
-        };
-    }, []);
-
     return (
         <>
-            <h1>{task.descrition}</h1>
+            <h1>{task.description}</h1>
             <p>{task.isCompleted ? "Completa" : "Não Completa"}</p>
         </>
     );
